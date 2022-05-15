faca08b1982c2ca648b14160811d28388946960b
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-05-15 12:24:44 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,23 @@
+img="";
+status="";
+
+function preload(){
+    img=loadImage('bedroom.jpg');
+}
+function setup(){
+    canvas=createCanvas()
+    canvas.center(640,420);
+
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
